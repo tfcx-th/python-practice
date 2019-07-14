c9d30392d05c6eb6a02155eba197555f1dd35c26
tfcx-th <tonghao_xjtu@qq.com>
2019-07-14 18:35:43 +0800

    weather data analysis

---
+++ weather-data-analysis/main.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from dateutil import parser
+from pandas.plotting import register_matplotlib_converters
+from sklearn.svm import SVR
+register_matplotlib_converters()
+
+df_ferrara = pd.read_csv('WeatherData/ferrara_270615.csv')
+df_milano = pd.read_csv('WeatherData/milano_270615.csv')
+df_mantova = pd.read_csv('WeatherData/mantova_270615.csv')
+df_ravenna = pd.read_csv('WeatherData/ravenna_270615.csv')
+df_torino = pd.read_csv('WeatherData/torino_270615.csv')
+df_asti = pd.read_csv('WeatherData/asti_270615.csv')
+df_bologna = pd.read_csv('WeatherData/bologna_270615.csv')
+df_piacenza = pd.read_csv('WeatherData/piacenza_270615.csv')
+df_cesena = pd.read_csv('WeatherData/cesena_270615.csv')
+df_faenza = pd.read_csv('WeatherData/faenza_270615.csv')
+
+dist = [
+    df_ravenna['dist'][0],
+    df_cesena['dist'][0],
+    df_faenza['dist'][0],
+    df_ferrara['dist'][0],
+    df_bologna['dist'][0],
+    df_mantova['dist'][0],
+    df_piacenza['dist'][0],
+    df_milano['dist'][0],
+    df_asti['dist'][0],
+    df_torino['dist'][0]
+]
+
+temp_max = [
+    df_ravenna['temp'].max(),
+    df_cesena['temp'].max(),
+    df_faenza['temp'].max(),
+    df_ferrara['temp'].max(),
+    df_bologna['temp'].max(),
+    df_mantova['temp'].max(),
+    df_piacenza['temp'].max(),
+    df_milano['temp'].max(),
+    df_asti['temp'].max(),
+    df_torino['temp'].max()
+]
+temp_min = [
+    df_ravenna['temp'].min(),
+    df_cesena['temp'].min(),
+    df_faenza['temp'].min(),
+    df_ferrara['temp'].min(),
+    df_bologna['temp'].min(),
+    df_mantova['temp'].min(),
+    df_piacenza['temp'].min(),
+    df_milano['temp'].min(),
+    df_asti['temp'].min(),
+    df_torino['temp'].min()
+]
+
+# 靠近海
+dist1 = dist[0:5]
+temp_max1 = temp_max[0:5]
+# 远离海
+dist2 = dist[5:10]
+temp_max2 = temp_max[5:10]
+
+svr_lin1 = SVR(kernel='linear', C=1e3)
+svr_lin2 = SVR(kernel='linear', C=1e3)
+
+fig, ax = plt.subplots()
+ax.plot(dist, temp_max, 'ro')
+
+plt.show()
